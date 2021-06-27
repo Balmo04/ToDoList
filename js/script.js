06e20bf5708130e2ee157d225c65d9506e0a8194
Emerson04-sv <balmore04ebao@gmail.com>
2021-06-27 16:44:36 -0600

    ya imprime las tareas

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const tabToSee = document.getElementById("view-task");
 /* INPUTS */
 const taskName = document.getElementById("title");
 const taskTime = document.getElementById("time");
-const priority = document.getElementsByClassName("btn-priority");
+const levelPriority = document.getElementsByClassName("btn-priority");
 const taskComment = document.getElementById("comment");
 /*SAVE*/
 const btnTask = document.getElementById("save");
@@ -13,6 +13,10 @@ const btnTask = document.getElementById("save");
 const msgError = document.getElementsByClassName("msg-error");
 /* MENSAJE DE GUARDADO */
 const msgSave = document.getElementById("msg-save");
+/* DIV DE TAREAS */
+const containerTask = document.getElementById("container-task");
+
+let positionRadio;
 
 for (let i = 0; i < btnNav.length; i++) {
    btnNav[i].addEventListener("click", function(){
@@ -29,12 +33,11 @@ for (let i = 0; i < btnNav.length; i++) {
 
 btnTask.addEventListener("click", function(){
     let isContent = validateInputs();
-    let isSelected = validateRadioButtons();
+    let isSelected = pressRadioButton();
     if(isContent && isSelected){ 
+        saveMessage(taskName.value, taskTime.value, positionRadio, taskComment.value);
         msgSave.classList.replace("hidden", "flex");
-        createTask();
-        setTimeout(()=>{
-            saveMessage();
+        setTimeout(()=>{  
             resetForm()
         },750)
     }
@@ -44,7 +47,7 @@ function validateInputs (){
     let temp=[];
     if(taskName.value){
         msgError[0].classList.replace("flex", "hidden");
-        temp[0]=true
+        temp[0]=true;
     }
     else{
         msgError[0].classList.replace("hidden", "flex");
@@ -52,7 +55,7 @@ function validateInputs (){
     }
     if(taskTime.value) {
         msgError[1].classList.replace("flex", "hidden");
-        temp[1]=true
+        temp[1]=true;
     }
     else{
         msgError[1].classList.replace("hidden", "flex");
@@ -60,38 +63,86 @@ function validateInputs (){
     }
     if(taskComment.value) {
         msgError[3].classList.replace("flex", "hidden");
-        temp[2]=true
+        temp[2]=true;
     }
     else{
         msgError[3].classList.replace("hidden", "flex");
         temp[2]=false;
     }
-    return ((temp[0]&&temp[1]&&temp[2]))
+    return  (temp[0]&&temp[1]&&temp[2]);
 }
 
-function validateRadioButtons(){
-    for (let i = 0; i < priority.length; i++) {
-        if(priority[i].checked){
-            msgError[2].classList.replace("flex", "hidden");
-            return true;
+function pressRadioButton(){
+    for (let i = 0; i < levelPriority.length; i++) {
+        if(levelPriority[i].checked){
+            positionRadio=i;
+            msgError[2].classList.replace("flex", "hidden"); 
+            return true;   
         }
-        else{
-            msgError[2].classList.replace("hidden", "flex");
+        else{  
+            msgError[2].classList.replace("hidden", "flex"); 
         }  
     }
 }
 
-function saveMessage(){
+function saveMessage(title, time, priority, comment){
+    imprintTask(title, time, priority, comment);
+   
+}
+
+function resetForm(){
+    taskName.value="";
+    taskTime.value="";
+    for (let i = 0; i <levelPriority.length; i++) { 
+       levelPriority[i].checked=false;
+    }
+    taskComment.value="";
     msgSave.classList.replace("flex", "hidden");
 }
-function resetForm(){
-    
-}
 
-function createTask(){
-
+function imprintTask(title, time, priority, comment){
+   switch (priority){
+        case 0:
+           containerTask.innerHTML+=`
+           <div class="border-task">
+                <hgroup class="bg-high flex task-type-style">
+                    <h4>${time}</h4>
+                    <h4>Alta</h4>
+                </hgroup>
+                <article>
+                    <h2 class="task-title-style">${title}</h2>
+                    <p class="task-description-style">${comment}</p>
+                </article>
+            </div>
+           `;
+           break;
+        case 1:
+            containerTask.innerHTML+=`
+            <div class="border-task">
+            <hgroup class="bg-medium flex task-type-style">
+                <h4>${time}</h4>
+                <h4>Media</h4>
+            </hgroup>
+            <article>
+                <h2 class="task-title-style">${title}</h2>
+                <p class="task-description-style">${comment}</p>
+            </article>
+             </div> 
+            `;
+            break;
+        case 2:
+                containerTask.innerHTML+=`
+                <div class="border-task">
+                <hgroup class="bg-low flex task-type-style">
+                    <h4>${time}</h4>
+                    <h4>Baja</h4>
+                </hgroup>
+                <article>
+                    <h2 class="task-title-style">${title}</h2>
+                    <p class="task-description-style">${comment}</p>
+                </article>
+                 </div>
+               `
+            break;
+   }
 }
-/* for (let i = 0; i < array.length; i++) {
-   
-    
-} */
